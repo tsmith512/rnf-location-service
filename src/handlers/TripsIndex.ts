6a82378dca41b542c4f9f6992853e91b9e1ab8da
Taylor Smith <tsmith@tsmithcreative.com>
2021-09-04 15:51:02 -0500

    Handle ranged request and errors

--- src/handlers/TripsIndex.ts
+++ src/handlers/TripsIndex.ts
@@ -6,22 +6,43 @@ const standardHeaders = new Headers({
   'Content-Type': 'application/json',
 });
 
-
 // NB: fetch() requires either a Host header or to fetch by hostname, not IP.
 // Also NB: random IP:Port combos not allowed. Must connect to a standard HTTP(S)
 // port on a named host in Cloudflare Worker world.
-async function getAllTrips(): Promise<Trip[]> {
-  return fetch(`${DB_ENDPOINT}/trips?select=id,label,slug,start,end`)
+async function getAllTrips(range: string | null): Promise<Trip[] | Error> {
+  const requestHeaders = new Headers();
+  if (range) { requestHeaders.append('Range', range); }
+
+  return fetch(`${DB_ENDPOINT}/trips?select=id,label,slug,start,end`, {headers: requestHeaders})
     .then(response => response.json())
     .then(payload => {
       return payload as Trip[];
+    })
+    .catch(error => {
+      if (error instanceof SyntaxError) {
+        return Error("JSON Parse Error");
+      }
+
+      // @TODO: Record and translate other errors here.
+      console.log(error);
+
+      return Error("Unknown error in getAllTrips");
     });
 }
 
 export async function TripsIndex(request: Request): Promise<Response> {
-  const trips = await getAllTrips();
+  // @TODO: Validate
+  const range = request.headers.get('Range');
 
-  console.log(trips);
+  const trips = await getAllTrips(range);
 
-  return new Response(JSON.stringify(trips), {headers: standardHeaders});
+  if (trips instanceof Error) {
+    return new Response(JSON.stringify({message: trips.message}), {status: 500, headers: standardHeaders});
+  }
+
+  if (!trips.length) {
+    return new Response(JSON.stringify({message: "No trips available in this range"}), {status: 416, headers: standardHeaders});
+  }
+
+  return new Response(JSON.stringify(trips), {status: 200, headers: standardHeaders});
 }
