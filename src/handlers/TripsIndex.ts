9613cfb5e8d6f4d5be06e02af1b8901a6a045499
Taylor Smith <tsmith@tsmithcreative.com>
2021-09-04 15:06:14 -0500

    Get the trip index endpoint working yayyy

--- src/handlers/TripsIndex.ts
+++ src/handlers/TripsIndex.ts
@@ -1,7 +1,17 @@
 import { Trip, TripProps } from '../lib/Trip';
 
-function getAllTrips(): Promise<Trip[]> {
-  return fetch('${DB_ENDPOINT}/trips?select=id,label,slug,start,end')
+// @TODO: How to make this everywhere?
+const standardHeaders = new Headers({
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
+});
+
+
+// NB: fetch() requires either a Host header or to fetch by hostname, not IP.
+// Also NB: random IP:Port combos not allowed. Must connect to a standard HTTP(S)
+// port on a named host in Cloudflare Worker world.
+async function getAllTrips(): Promise<Trip[]> {
+  return fetch(`${DB_ENDPOINT}/trips?select=id,label,slug,start,end`)
     .then(response => response.json())
     .then(payload => {
       return payload as Trip[];
@@ -13,5 +23,5 @@ export async function TripsIndex(request: Request): Promise<Response> {
 
   console.log(trips);
 
-  return new Response(JSON.stringify(trips));
+  return new Response(JSON.stringify(trips), {headers: standardHeaders});
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+declare global {
+  const DB_ENDPOINT: string;
+}
+
 import { routeRequest } from './router';
 
 addEventListener('fetch', (event) => {
