f422fbb7828131bcd08f8beec37d688eb6a317d8
Taylor Smith <tsmith@tsmithcreative.com>
2021-09-20 09:35:03 -0500

    Had some trouble with CORS again, adding in request headers because browsers were checking content-type

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -11,6 +11,7 @@ export interface RNFRequest extends Request {
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': '*',
   'Access-Control-Max-Age': '86400',
 };
 
