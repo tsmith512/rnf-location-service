c564b15aadb0881f5d4bfd84cd24a20323cc0126
Taylor Smith <tsmith@tsmithcreative.com>
2021-10-12 00:37:12 -0500

    Hotfix: Fighting with CORS. Allow all inbound headers.

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -11,7 +11,7 @@ export interface RNFRequest extends Request {
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Authorization',
+  'Access-Control-Allow-Headers': '*',
   'Access-Control-Max-Age': '86400',
 };
 
